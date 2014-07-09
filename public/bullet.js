0a5c916836be6bcacb823a25745dab5e6f0fec83
Arieh Smith <arieh.smith@gmail.com>
2014-07-09 14:32:07 -0400

    transfer to Rails app

---
+++ public/bullet.js
@@ -0,0 +1,89 @@
+(function (root) {
+  var Asteroids = root.Asteroids = (root.Asteroids || {});
+  
+  function drawRotatedImage(ctx, image, x, y, angle) {
+  	ctx.save();
+  	ctx.translate(x, y);
+  	ctx.rotate(angle);
+  	ctx.drawImage(image, -(image.width/2), -(image.height/2));
+  	ctx.restore(); 
+  }
+  
+  Function.prototype.inherits = function(parent){
+    function Surrogate() {}
+    Surrogate.prototype = parent.prototype;
+    this.prototype = new Surrogate (); 
+  }
+  
+  var COLOR = "white";
+  var RADIUS = 10;
+  
+  var Bullet = Asteroids.Bullet = function (pos, vel) {
+    Asteroids.MovingObject.call(this, pos, vel, RADIUS, COLOR);
+  }
+  
+  Bullet.inherits(Asteroids.MovingObject);
+  
+  Bullet.prototype.draw = function (ctx) {
+    var that = this;
+    
+    drawRotatedImage(ctx, Asteroids.Game.missile, that.xAxis, that.yAxis, Math.atan2(this.vx, -this.vy));
+    
+    
+    ctx.fillStyle = this.col;
+    ctx.beginPath();
+
+    ctx.arc(
+      this.xAxis,
+      this.yAxis,
+      this.rad,
+      0,
+      2 * Math.PI,
+      false
+    );
+  };
+  
+  Bullet.prototype.move = function (maxX, maxY) {
+    this.xAxis = ((this.xAxis + this.vx));
+    this.yAxis = ((this.yAxis + this.vy));   
+
+    if (this.xAxis <= 0) {
+      this.xAxis = maxX
+    }
+    if (this.yAxis <= 0) {
+      this.yAxis = maxY
+    }
+    console.log(this.xAxis);
+  }
+  
+  
+  Bullet.createBullet = function (pos, vel){
+
+    var dx, dy;
+    
+    if (vel[0] === 0 && vel[1] === 0) { return null; }
+    
+		var shipSpeed = Math.pow(vel[0], 2) + Math.pow(vel[1], 2);
+		var bulletSpeed = 1300;
+		var bulletFactor = bulletSpeed / shipSpeed;
+				
+    dx = Math.sqrt(Math.pow(vel[0], 2) * bulletFactor);
+    dy = Math.sqrt(Math.pow(vel[1], 2) * bulletFactor);
+    
+		if (vel[0] < 0) dx = dx * -1;
+		if (vel[1] < 0) dy = dy * -1;
+		
+    return new Bullet(
+      [
+        pos[0],
+        pos[1]
+      ],
+      [
+        dx,
+        dy
+      ]
+    );
+  }
+  
+  
+})(this);
